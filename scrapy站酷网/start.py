0be698bae3726e3dacfee68a4aae6c76516a835f
南城义少 <byack-hkc@foxmail.com>
2021-07-30 18:15:46 +0800

    站酷爬取

--- scrapy站酷网/start.py
+++ scrapy站酷网/start.py
@@ -2,10 +2,3 @@ from scrapy import cmdline
 
 
 cmdline.execute("scrapy crawl zcool_spider".split(" "))
-
-# import os
-
-# dir = os.path.join(os.path.dirname(__file__), "images/1")
-
-# if not os.path.exists(dir):
-#     os.mkdir(dir)
